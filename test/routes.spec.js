88a025c2ba433b057364fe4e9bc2c2892973847b
Matthew Gordon <laxredemption5525@gmail.com>
2016-12-22 20:30:09 -0700

    Add test skeleton

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,1 +1,14 @@
 'use strict';
+
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../app');
+
+chai.use(chaiHttp);
+
+describe('API Routes', () => {
+  
+});
