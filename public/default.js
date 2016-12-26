80f733f63457020271ea39cc39e0ae2d50dc4a3e
Matthew Gordon <laxredemption5525@gmail.com>
2016-12-26 16:17:58 -0700

    Add DELETE functionality

--- public/default.js
+++ public/default.js
@@ -7,12 +7,18 @@ $(document).ready(() => {
   const $newName = $('#newName');
   const $newEmail = $('#newEmail');
   const $newBio = $('#newBio');
+  const $updateId = $('#updateId');
+  const $updateName = $('#updateName');
+  const $updateEmail = $('#updateEmail');
+  const $updateBio = $('#updateBio');
+  const $deleteId = $('#deleteId');
   const $getAll = $('#getAll');
   const $getSingle = $('#getSingle');
   const $createItem = $('#createItem');
   const $updateItem = $('#updateItem');
   const $deleteItem = $('#deleteItem');
 
+  // GET all items
   $getAll.click(() => {
     $.ajax({
       url: 'api/v1/items',
@@ -33,7 +39,7 @@ $(document).ready(() => {
     });
   });
 
-  // GET single message
+  // GET single item
   $getSingle.click(() => {
     let id = $singleId.val();
 
@@ -53,6 +59,7 @@ $(document).ready(() => {
     });
   });
 
+  // POST create new item
   $createItem.click(() => {
     let name = $newName.val();
     let email = $newEmail.val();
@@ -67,16 +74,92 @@ $(document).ready(() => {
       data: JSON.stringify(body)
     })
     .done(() => {
-      console.log('Success!');
+      console.log('Success created!');
+      $.ajax({
+        url: 'api/v1/items',
+        type: 'GET'
+      })
+      .done((items) => {
+        $display.empty();
+        $.each(items, (index, value) => {
+          $display.append(`
+            <tr>
+              <td>${value.id}</td>
+              <td>${value.name}</td>
+              <td>${value.email}</td>
+              <td>${value.bio}</td>
+            </tr>
+          `);
+        });
+      });
     });
   });
 
+  // PUT update item
   $updateItem.click(() => {
-    console.log('Clicked Update Item');
+    let id = $updateId.val();
+    let name = $updateName.val();
+    let email = $updateEmail.val();
+    let bio = $updateBio.val();
+    let body = { "name": name, "email": email, "bio": bio };
+
+    $.ajax({
+      url: `/api/v1/items/${id}`,
+      type: 'PUT',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify(body)
+    })
+    .done(() => {
+      console.log('Updated Successfully!');
+      $.ajax({
+        url: 'api/v1/items',
+        type: 'GET'
+      })
+      .done((items) => {
+        $display.empty();
+        $.each(items, (index, value) => {
+          $display.append(`
+            <tr>
+              <td>${value.id}</td>
+              <td>${value.name}</td>
+              <td>${value.email}</td>
+              <td>${value.bio}</td>
+            </tr>
+          `);
+        });
+      });
+    });
   });
 
+  // DELETE item
   $deleteItem.click(() => {
-    console.log('Clicked Delete Item');
+    let id = $deleteId.val();
+
+    $.ajax({
+      url: `api/v1/items/${id}`,
+      type: 'DELETE'
+    })
+    .done(() => {
+      console.log('Deleted Successfully!');
+      $.ajax({
+        url: 'api/v1/items',
+        type: 'GET'
+      })
+      .done((items) => {
+        $display.empty();
+        $.each(items, (index, value) => {
+          $display.append(`
+            <tr>
+              <td>${value.id}</td>
+              <td>${value.name}</td>
+              <td>${value.email}</td>
+              <td>${value.bio}</td>
+            </tr>
+          `);
+        });
+      });
+    });
   });
 
 });
