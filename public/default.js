06ddc149f933a1ac66a428c805c389ceb2b6a296
Matthew Gordon <laxredemption5525@gmail.com>
2016-12-26 15:09:28 -0700

    Link buttons for test

--- public/default.js
+++ public/default.js
@@ -3,4 +3,26 @@
 
 $(document).ready(() => {
   const $getAll = $('#getAll');
+  const $getSingle = $('#getSingle');
+  const $createItem = $('#createItem');
+  const $updateItem = $('#updateItem');
+  const $deleteItem = $('#deleteItem');
+
+  $getAll.click(() => {
+    console.log('Clicked GET ALL');
+  });
+
+  $getSingle.click(() => {
+    console.log('Clicked GET Single');
+  });
+  $createItem.click(() => {
+    console.log('Clicked Create Item');
+  });
+  $updateItem.click(() => {
+    console.log('Clicked Update Item');
+  });
+  $deleteItem.click(() => {
+    console.log('Clicked Delete Item');
+  });
+
 });
