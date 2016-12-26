302ff0bdb98457e629ddd4f2e47237098092c6a4
Matthew Gordon <laxredemption5525@gmail.com>
2016-12-26 15:21:15 -0700

    GET call added, linked to display

--- public/default.js
+++ public/default.js
@@ -2,6 +2,7 @@
 
 
 $(document).ready(() => {
+  const $display = $('#display');
   const $getAll = $('#getAll');
   const $getSingle = $('#getSingle');
   const $createItem = $('#createItem');
@@ -9,7 +10,23 @@ $(document).ready(() => {
   const $deleteItem = $('#deleteItem');
 
   $getAll.click(() => {
-    console.log('Clicked GET ALL');
+    $.ajax({
+      url: '/api/v1/items',
+      type: 'GET'
+    })
+    .done((items) => {
+      $display.empty();
+      $.each(items, (index, value) => {
+        $display.append(`
+          <tr>
+            <td>${value.id}</td>
+            <td>${value.name}</td>
+            <td>${value.email}</td>
+            <td>${value.bio}</td>
+          </tr>
+        `);
+      });
+    });
   });
 
   $getSingle.click(() => {
